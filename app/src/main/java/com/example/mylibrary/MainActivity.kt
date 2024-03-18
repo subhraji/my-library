5269bf8d9dbe59b98131d27fa09a7e934284b07d
subhrajitdeb <subhrajitdeb54@gmail.com>
2024-03-18 23:54:46 +0530

    first commit

---
+++ app/src/main/java/com/example/mylibrary/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.mylibrary
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.image_preview.ImagePreview
+import com.example.mylibrary.ui.theme.MyLibraryTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyLibraryTheme {
+                Box(modifier = Modifier.fillMaxSize()) {
+                    ImagePreview(
+                        image = painterResource(id = R.drawable.iconn),
+                        description = "Hello World",
+                        modifier = Modifier
+                            .align(Alignment.Center)
+                            .size(150.dp)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MyLibraryTheme {
+        Greeting("Android")
+    }
+}
